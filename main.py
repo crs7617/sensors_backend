8bf6657fa7c9dcdd42fb5938248359c1d1dba7c9
Sairam Cr <crsairam7617@gmail.com>
2024-10-24 22:49:28 +0530

    render confg

--- main.py
+++ main.py
@@ -69,7 +69,10 @@ async def predict_weather(data: SensorData):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# For local testing
+        
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    
+    port = int(os.getenv("PORT", 8000))  # Render will provide a PORT, default to 8000 for local testing
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
